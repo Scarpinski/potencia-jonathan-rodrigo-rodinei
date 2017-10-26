4a50f28897ed65c45a3e85357fd7562690a2473e
jonathanantunes <31494131+jonathanantunes@users.noreply.github.com>
2017-10-26 14:41:00 -0200

    tentativa1

--- ufrgs_potencia.m
+++ ufrgs_potencia.m
@@ -2,3 +2,12 @@ function [lambda,x]=ufrgs_potencia(A,x,N)
 % Acha o autovalor dominante de A e seu autovetor correspondente.
 %
 % [lambda,x]=ufrgs_potencia(A,x,N)
+
+for i=1:N
+    
+    x=(A*x)/norm(A*x)
+    
+end
+
+lambda=(x')*A*x
+end
